7a61637dfa48205ae24c3934a3c2df646253929d
Abdullah <abdulluhodulate@gmail.com>
2024-01-11 08:11:21 +0100

    perimeterOfRectangle, volumeOfRectPrism, circumOfCircle

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -1,14 +1,16 @@
-// function myfullName() {
+// function myfullName() {				-----1
 // 	console.log("Abdullah Odulate");
 // }
 
 function myfullName(firstName, lastName) {
+	// 2
 	return `${firstName} ${lastName}`;
 }
 
 console.log(myfullName());
 
 function addNumber(num1, num2) {
+	// 3
 	let sum = num1 + num1;
 	return sum;
 }
@@ -16,8 +18,34 @@ function addNumber(num1, num2) {
 console.log(addNumber());
 
 function areaOfRectangle(length, width) {
+	// 4
 	const area = length * width;
 	return area;
 }
 
 console.log(areaOfRectangle());
+
+function perimeterOfRectangle(length, width) {
+	// 5
+	const area = 2 * (length + width);
+	return area;
+}
+
+console.log(areaOfRectangle());
+
+function volumeOfRectPrism(length, width, weight) {
+	// 6
+	const volume = weight * length * width;
+	return volume;
+}
+
+console.log(volumeOfRectPrism());
+
+function circumOfCircle(r) {
+	// 6
+	const π = Math.PI;
+	const area = π * r * r;
+	return area;
+}
+
+console.log(circumOfCircle());
