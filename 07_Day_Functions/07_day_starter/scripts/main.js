de013fbd64534da12e5b541d688bd917544cc8b3
Abdullah <abdulluhodulate@gmail.com>
2024-01-11 08:01:38 +0100

    myFulllName, addNumber, areaOfRectangle

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -1,3 +1,23 @@
-function myfullName() {
-	console.log("Abdullah Odulate");
+// function myfullName() {
+// 	console.log("Abdullah Odulate");
+// }
+
+function myfullName(firstName, lastName) {
+	return `${firstName} ${lastName}`;
 }
+
+console.log(myfullName());
+
+function addNumber(num1, num2) {
+	let sum = num1 + num1;
+	return sum;
+}
+
+console.log(addNumber());
+
+function areaOfRectangle(length, width) {
+	const area = length * width;
+	return area;
+}
+
+console.log(areaOfRectangle());
